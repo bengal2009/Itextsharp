0b0cc9c055a260c72df4e52981fdd8d908fb6127
benny <bennylin@outlook.com>
2018-06-28 18:24:54 +0800

    Office

--- Itextsharp/Form1.vb
+++ Itextsharp/Form1.vb
@@ -26,7 +26,12 @@ Public Class Form1
 
                     ''//Insert a blank page
                     Doc.NewPage()
-
+                    Doc.AddTitle("Hello World example")
+                    Doc.AddSubject("This is an Example 4 of Chapter 1 of Book 'iText in Action'")
+                    Doc.AddKeywords("Metadata, iTextSharp 5.4.4, Chapter 1, Tutorial")
+                    Doc.AddCreator("iTextSharp 5.4.4")
+                    Doc.AddAuthor("Debopam Pal")
+                    Doc.AddHeader("Nothing", "No Header")
                     ''//Add a simple paragraph with text
                     Doc.Add(New Paragraph("Hello World", Font))
                     Doc.Add(New Paragraph("這是測試", Font))
@@ -41,4 +46,91 @@ Public Class Form1
 
 
     End Sub
+
+    Public Sub ReplacePDFText(ByVal strSearch As String, ByVal scCase As StringComparison, ByVal strSource As String, ByVal strDest As String)
+
+        Dim psStamp As PdfStamper = Nothing 'PDF Stamper Object
+        Dim pcbContent As PdfContentByte = Nothing 'Read PDF Content
+
+        If File.Exists(strSource) Then 'Check If File Exists
+
+            Dim pdfFileReader As New PdfReader(strSource) 'Read Our File
+
+            psStamp = New PdfStamper(pdfFileReader, New FileStream(strDest, FileMode.Create)) 'Read Underlying Content of PDF File
+
+            pbProgress.Value = 0 'Set Progressbar Minimum Value
+            pbProgress.Maximum = pdfFileReader.NumberOfPages 'Set Progressbar Maximum Value
+
+            For intCurrPage As Integer = 1 To pdfFileReader.NumberOfPages 'Loop Through All Pages
+
+                Dim lteStrategy As LocTextExtractionStrategy = New LocTextExtractionStrategy 'Read PDF File Content Blocks
+
+                pcbContent = psStamp.GetUnderContent(intCurrPage) 'Look At Current Block
+
+                'Determine Spacing of Block To See If It Matches Our Search String
+                lteStrategy.UndercontentCharacterSpacing = pcbContent.CharacterSpacing
+                lteStrategy.UndercontentHorizontalScaling = pcbContent.HorizontalScaling
+
+                'Trigger The Block Reading Process
+                Dim currentText As String = PdfTextExtractor.GetTextFromPage(pdfFileReader, intCurrPage, lteStrategy)
+
+                'Determine Match(es)
+                Dim lstMatches As List(Of Itextsharp.text.Rectangle) = lteStrategy.GetTextLocations(strSearch, scCase)
+
+                Dim pdLayer As PdfLayer 'Create New Layer
+                pdLayer = New PdfLayer("Overrite", psStamp.Writer) 'Enable Overwriting Capabilities
+
+                'Set Fill Colour Of Replacing Layer
+                pcbContent.SetColorFill(BaseColor.BLACK)
+
+                For Each rctRect As Rectangle In lstMatches 'Loop Through Each Match
+
+                    pcbContent.Rectangle(rctRect.Left, rctRect.Bottom, rctRect.Width, rctRect.Height) 'Create New Rectangle For Replacing Layer
+
+                    pcbContent.Fill() 'Fill With Colour Specified
+
+                    pcbContent.BeginLayer(pdLayer) 'Create Layer
+
+                    pcbContent.SetColorFill(BaseColor.BLACK) 'Fill aLyer
+
+                    pcbContent.Fill() 'Fill Underlying Content
+
+                    Dim pgState As PdfGState 'Create GState Object
+                    pgState = New PdfGState()
+
+                    pcbContent.SetGState(pgState) 'Set Current State
+
+                    pcbContent.SetColorFill(BaseColor.WHITE) 'Fill Letters
+
+                    pcbContent.BeginText() 'Start Text Replace Procedure
+
+                    pcbContent.SetTextMatrix(rctRect.Left, rctRect.Bottom) 'Get Text Location
+
+                    'Set New Font And Size
+                    pcbContent.SetFontAndSize(BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED), 9)
+
+                    pcbContent.ShowText("AMAZING!!!!") 'Replacing Text
+
+                    pcbContent.EndText() 'Stop Text Replace Procedure
+
+                    pcbContent.EndLayer() 'Stop Layer replace Procedure
+
+                Next
+
+                pbProgress.Value = pbProgress.Value + 1 'Increase Progressbar Value
+
+                pdfFileReader.Close() 'Close File
+
+            Next
+
+            psStamp.Close() 'Close Stamp Object
+
+        End If
+
+        'Add Watermark
+        AddPDFWatermark("C:\test_words_replaced.pdf", "C:\test_Watermarked_and_Replaced.pdf", Application.StartupPath & "\Anuba.jpg")
+
+    End Sub
+
+
 End Class
